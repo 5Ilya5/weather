463607a6eb3c0a554d1756e7638b76dba3cf4383
5Ilya5 <102946779+5Ilya5@users.noreply.github.com>
2022-08-25 09:24:23 +0300

    Add files via upload

---
+++ information.py
@@ -0,0 +1,11 @@
+try:
+    res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+                 params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+    data = res.json()
+    print("conditions:", data['weather'][0]['description'])
+    print("temp:", data['main']['temp'])
+    print("temp_min:", data['main']['temp_min'])
+    print("temp_max:", data['main']['temp_max'])
+except Exception as e:
+    print("Exception (weather):", e)
+    pass
---
+++ requests.py
@@ -0,0 +1,2 @@
+requests.get("http://api.openweathermap.org/data/2.5/find",
+                 params={'q': s_city, 'type': 'like', 'units': 'metric', 'APPID': appid})
